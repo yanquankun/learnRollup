cfa497b698ce09f7c2ee1ae0885230cb87ee84ca
yanquankun <17600610907@163.com>
2024-11-28 10:32:33 +0800

    feat: html build commit

--- rollup.html.js
+++ rollup.html.js
@@ -14,10 +14,13 @@ const isDev = env === "development";
 const del = require("rollup-plugin-delete");
 const path = require("path");
 const fs = require("fs");
+const { createHash } = require("crypto");
+const less = require("less");
 
-const less = require("rollup-plugin-less");
 const hash = require("rollup-plugin-hash");
 const html = require("./plugin/html-rollup-plugin");
+const postcss = require("rollup-plugin-postcss");
+const copy = require("rollup-plugin-copy");
 //====== start ======
 // 与 Webpack 和 Browserify 等其他打包程序不同，Rollup 默认不会识别node_modules中的依赖
 // resolve 识别node_modules中的依赖
@@ -63,11 +66,46 @@ module.exports = {
   plugins: [
     commonjs(),
     del({ targets: "dist/*", runOnce: true }),
-    less({
-      // include: ["html/css/index.css", "html/css/ls.less"],
-      output: "dist/css/styles.[hash].css",
+    // less({
+    //   include: ["html/css/*"],
+    //   output: "dist/css/[name].[hash].css",
+    // }),
+    copy({
+      targets: [
+        {
+          src: "html/css/**/*",
+          dest: "dist/css",
+          rename: (name, extension, fullPath) => {
+            const hash = createHash("md5")
+              .update(fs.readFileSync(path.resolve(fullPath)))
+              .digest("hex")
+              .slice(0, 8);
+            return `${name}-${hash}.css`;
+          },
+          transform: async (contents, filename) => {
+            if (filename.includes(".less")) {
+              const result = await less.render(contents.toString(), {
+                filename,
+              });
+              return result.css;
+            }
+            return contents;
+          },
+        },
+      ],
+      hook: "generateBundle",
     }),
-    hash(),
-    html({ title: "测试", compress: true }),
+    // hash(),
+    // postcss({
+    //   // inject: true,
+    //   extract: (chunk) => `dist/css/${chunk.name}-[hash].css`,
+    //   minimize: true,
+    //   extensions: [".css", ".less"],
+    // }),
+    hash({
+      dest: "dist",
+      replace: true,
+    }),
+    html({ title: "测试", compress: true, cssResourcePath: "css" }),
   ].concat(commonConfig.plugins),
 };
